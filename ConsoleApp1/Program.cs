7b84480409ad6c1d4b142998ae704283c4a04dec
squirrell2 <good.thankuu@gmail.com>
2024-06-14 11:20:46 +0300

    Код, сложнее, чем "Hello, World"

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Good Morning, World!");
-// Тёма - спящий просыпун
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Запрашиваем имя пользователя
+        Console.Write("Введите ваше имя: ");
+        string name = Console.ReadLine();
+
+        // Запрашиваем возраст пользователя
+        Console.Write("Введите ваш возраст: ");
+        int age;
+        while (!int.TryParse(Console.ReadLine(), out age) || age < 0)
+        {
+            Console.Write("Пожалуйста, введите корректный возраст: ");
+        }
+
+        // Выводим сообщение
+        Console.WriteLine($"Привет, {name}! Тебе {age} лет.");
+    }
+}
